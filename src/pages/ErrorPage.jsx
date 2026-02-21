2ae8e60361c66691350c7eea5aabbc3fe514fc98
anjil-rai <anjilrai133@gmail.com>
2026-02-21 22:42:45 +0545

    changes

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -10,7 +10,7 @@ const ErrorPage = () => {
         404 - Page Not Found
       </h1>
       <p className="text-gray-600 mb-8">
-        The page you're looking for doesn't exist or has been moved.
+        The page you're looking for doesn't exist 
       </p>
       <Link
         to="/"
